19f2ad28cd51a3eae27e79ca5dea75a1b99147c7
aprianfirlanda <aprianfirlanda@gmail.com>
2025-09-27 14:24:27 +0700

    feat(fe): protect the app with keycloak

--- react-keycloak-demo/src/main.jsx
+++ react-keycloak-demo/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import keycloak from "./keycloak.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+keycloak.init({ onLoad: "login-required" }).then((authenticated) => {
+  if (authenticated) {
+    createRoot(document.getElementById('root')).render(
+      <StrictMode>
+        <App keycloak={keycloak}/>
+      </StrictMode>,
+    )
+  }
+});
